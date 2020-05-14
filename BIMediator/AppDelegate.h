ccf57bf00bebab5dec10e988434e8e4be10b308b
yuxinwen <yuxinwen@baidu.com>
2020-05-14 15:15:43 +0800

    Initial Commit

---
+++ BIMediator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BIMediator
+//
+//  Created by xinwen on 2020/5/14.
+//  Copyright © 2020 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BIMediator/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  BIMediator
+//
+//  Created by xinwen on 2020/5/14.
+//  Copyright © 2020 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
